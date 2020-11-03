a6197ff4ebb51d0ed65058cda81116affcda3f1c
Marin Masic <mamasic@gmail.com>
2020-11-03 16:05:14 +0100

    - racun report: zaglavlje, br racuna, partner, mjesto rada

--- src/api/racun-module/reports/racun.js
+++ src/api/racun-module/reports/racun.js
@@ -1,11 +1,21 @@
 const Pdfkit = require('pdfkit');
+const dateformat = require('dateformat');
 
 function getRacunReport(firma, racun, partner) {
   const { naziv, adresa, mjesto, oib, pdv_id: pdvId, zr } = firma;
 
   const font = './fonts/LiberationSans-Regular.ttf';
   const fontBold = './fonts/LiberationSans-Bold.ttf';
-  const doc = new Pdfkit();
+
+  const pageConfig = {
+    margins: {
+      left: 50,
+      right: 50,
+      top: 50,
+      bottom: 80,
+    }
+  };
+  const doc = new Pdfkit(pageConfig);
   doc.fontSize(12)
     .font(font)
     .text(naziv)
@@ -23,9 +33,30 @@ function getRacunReport(firma, racun, partner) {
     .text(partner.adresa, partnerStyle)
     .text(`${partner.posta} ${partner.mjesto}`, partnerStyle);
 
-  doc.fontSize(12)
+  const {
+    datum,
+    status_id: status,
+    zaglavlje,
+    broj_racuna: brojRacuna,
+    mjesto_rada_naziv: mjestoRada,
+    mjesto_rada_adresa: adresaRada,
+  } = racun.racunGlava;
+
+  doc.fontSize(11)
     .font(font)
-    .text(racun.racunGlava.zaglavlje, partnerStyle);
+    .text(zaglavlje, partnerStyle);
+
+  doc.fontSize(11)
+    .font(fontBold)
+    .text(`Račun: ${brojRacuna}-P1-1`);
+
+  doc.fontSize(11)
+    .font(font)
+    .text(`OIB: ${partner.oib}`, 300, 255)
+    .text(`Datum: ${dateformat(datum, 'dd.mm.yyyy.')}`, 50, 270)
+    .text(`Mjesto rada: ${mjestoRada}`, 300, 270)
+    .text(status, 50, 285)
+    .text(`Adresa rada: ${adresaRada}`, 300, 285) ;
 
   return doc;
 }
