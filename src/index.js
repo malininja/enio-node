ef0d390ae1acb4e797cbcd5d86e0b71fef6aca50
malininja <mamasic@gmail.com>
2025-10-17 11:35:22 +0200

    Add start URL to index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const express = require("express");
 const apiRouter = require("./api");
 const homeRouter = require("./home-router");
-const path = require('path');
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -11,14 +11,15 @@ const server = http.Server(app);
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, "static")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/home', homeRouter);
+app.use("/home", homeRouter);
 app.use("/api", apiRouter);
 
 server.listen(port, () => {
   console.log(`Server slusa na ${port}`);
+  console.log("http://localhost:3000/home");
 });
